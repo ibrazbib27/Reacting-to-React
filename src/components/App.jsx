f0e8f8784ba67f7e5d736a01ea745f6e7dabb34e
ibrazbib27 <ibrahim.zbib@macys.com>
2020-07-28 16:12:18 -0400

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 /*
 const App = (props) => {
@@ -7,47 +7,47 @@ const App = (props) => {
     );
 }
 */
-class App extends Component{
-    constructor(){
-        super();
-        this.state = {
-            text: "Wednesday",
-            hasLoaded: false
-        }
-   
-    }
-    handleInputChange = (value) =>{
-        this.setState({ response: value
-                      });
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "Wednesday",
+      hasLoaded: false,
     };
-    handleBtnEvent = (e) =>{
-        this.setState({ hasLoaded: true  });
-    };
-    componentDidMount = () => {
-        this.setState({ hasLoaded: true  });
-    };
-    newStateTextValue = () =>{
-        this.state.text = this.props.day;
-    };
- 
-    
-render(){
+  }
+  handleInputChange = (value) => {
+    this.setState({ response: value });
+  };
+  handleBtnEvent = (e) => {
+    this.setState({ hasLoaded: true });
+  };
+  componentDidMount = () => {
+    this.setState({ hasLoaded: true });
+  };
+  newStateTextValue = () => {
+    this.state.text = this.props.day;
+  };
+
+  render() {
     this.newStateTextValue();
     this.state.text = "String Again";
     let trueHeader = `${this.props.day} ${this.state.text}`;
-  return (
+    return (
       <React.Fragment>
-      <h1>{this.state.hasLoaded ?  trueHeader : "Loading..."}</h1>
-    
-      <input type={this.state.hasLoaded ? "text" : "hidden"} placeholder={this.props.day} onChange={(e) => {
-              this.handleInputChange(e.target.value);
-          }} />
-         {this.state.hasLoaded ? <br /> : null} 
+        <h1>{this.state.hasLoaded ? trueHeader : "Loading..."}</h1>
+
+        <input
+          type={this.state.hasLoaded ? "text" : "hidden"}
+          placeholder={this.props.day}
+          onChange={(e) => {
+            this.handleInputChange(e.target.value);
+          }}
+        />
+        {this.state.hasLoaded ? <br /> : null}
         <button onClick={this.handleBtnEvent}>Click Here!</button>
       </React.Fragment>
-      
     );
-    }
+  }
 }
 
 export default App;
